a8940cc2667dac1562b88a123c524f135b07df87
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2025-04-08 16:19:50 +0530

    UI changes

---
+++ client/src/utils/navigation.ts
@@ -0,0 +1,43 @@
+import {
+  Squares2X2Icon,
+  UserIcon,
+  CalendarIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  ChartBarIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/24/outline";
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: Squares2X2Icon,
+  },
+  {
+    name: "Counselors",
+    href: "/dashboard/counselors",
+    icon: UserIcon,
+  },
+  {
+    name: "Sessions",
+    href: "/dashboard/sessions",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Messages",
+    href: "/dashboard/messages",
+    icon: ChatBubbleOvalLeftEllipsisIcon,
+  },
+  {
+    name: "Profile",
+    href: "/dashboard/profile",
+    icon: ChartBarIcon,
+  },
+  {
+    name: "Settings",
+    href: "/dashboard/settings",
+    icon: Cog6ToothIcon,
+  },
+];
+
+export default navigation;
