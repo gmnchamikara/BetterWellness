374650cf60ab2229f5ddf14f5cb701be3b620225
Nuwan Chamikara <www.gmnuwanchamikara@gmail.com>
2025-04-13 19:08:02 +0530

    redux store and related file has done

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,43 +1,14 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "@/store/features/users/userSlice";
 
-// RootState type will be inferred from this
-const rootReducer = combineReducers({
-  user: userReducer,
-});
-
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
+// Configure the Redux store
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Needed for redux-persist
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    user: userReducer,
+  },
+  // optional: add middleware or devTools config here
 });
 
-// Used in `_app.tsx` for wrapping with <PersistGate />
-export const persistor = persistStore(store);
-
-// Infer types for dispatch and selector usage
+// Infer types for usage in components
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
